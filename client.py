a225bd8c8736fd3734c1581a98e2dcc3073b96b7
Augusto Ciuffoletti <augusto.ciuffoletti@unipi.it>
2020-12-17 16:14:41 +0100

    Transizione a Python3

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
-host = raw_input('Scegli un host: ')
-port = raw_input('Scegli una porta: ')
+host = input('Scegli un host: ')
+port = input('Scegli una porta: ')
 buflen = 80
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creazione del socket 
 s.connect((host,int(port)))                            # Connessione (3-way handsake)
-msg = raw_input('Stringa da spedire: ')
-s.send(msg)                                            # Spedizione messaggio
-data = s.recv(buflen)                                  # Attesa risposta
+msg = input('Stringa da spedire: ')
+s.send(msg.encode('utf8'))                                            # Spedizione messaggio
+data = s.recv(buflen).decode('utf8')                                  # Attesa risposta
 print('Stringa ricevuta: '+data)
 s.close()                                              # Chiusura socket
